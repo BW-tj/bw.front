99f109af2686cb515f2244935670ed15957f01dd
DalerAlyamov <developer.daler@gmail.com>
2022-02-14 18:09:21 +0500

    updated runner file

--- _run.sh
+++ _run.sh
@@ -2,5 +2,7 @@ npm i
 echo "all packages installed"
 npm run build
 echo "app built"
+echo "remove old version"
+pm2 stop bw-client && pm2 delete bw-client
 echo "starting to run app ....."
 npm run pm2 
