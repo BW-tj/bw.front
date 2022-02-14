b8ba64e6a42e76031ce0a6431317a54d5b01b61f
Sirius Sufiev <44986984+sc0der@users.noreply.github.com>
2022-02-14 18:29:51 +0400

    Update _run.sh

--- _run.sh
+++ _run.sh
@@ -6,7 +6,5 @@ echo "all packages installed"
 mv Layouts/ layouts/
 npm run build
 echo "app built"
-echo "remove old version"
-pm2 stop bw-client && pm2 delete bw-client
 echo "starting to run app ....."
 npm start
