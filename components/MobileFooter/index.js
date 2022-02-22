4427bbea62cf5543d2fc15cfe2915ae1780669ad
DalerAlyamov <developer.daler@gmail.com>
2022-02-22 15:34:26 +0500

    add notifications in MobileFooter

--- components/MobileFooter/index.js
+++ components/MobileFooter/index.js
@@ -4,10 +4,15 @@ import styles from './index.module.scss'
 import { BxHomeAlt, FavoriteBorder, PersonOutline, ShoppingCart } from '../../icons';
 import { useRouter } from 'next/dist/client/router';
 import classNames from 'classnames';
+import { useSelector } from 'react-redux';
 
 const MobileFooter = () => {
 
 	const router = useRouter();
+
+	const cart = useSelector(state => state.cart)
+
+	const favorites = useSelector(state => state.favorites)
 
 	return (
 		<div className={styles.root}>
@@ -27,6 +32,11 @@ const MobileFooter = () => {
 				<a className={classNames(styles.item, router.pathname === '/favorites' && styles.active)}>
 					<div className={styles.item__icon}>
 						<FavoriteBorder />
+						{favorites.length !== 0 &&
+							<div className={classNames(styles.item__iconInfo, styles.item__iconInfo_favorites)}>
+								{favorites.length}
+							</div>
+						}
 					</div>
 					<div className={styles.item__text}>
 						Избранные
@@ -38,6 +48,11 @@ const MobileFooter = () => {
 				<a className={classNames(styles.item, router.pathname === '/cart' && styles.active)}>
 					<div className={styles.item__icon}>
 						<ShoppingCart />
+						{cart.length !== 0 &&
+							<div className={classNames(styles.item__iconInfo, styles.item__iconInfo_favorites)}>
+								{cart.reduce((acc, cur) => acc + cur.count, 0)}
+							</div>
+						}
 					</div>
 					<div className={styles.item__text}>
 						Корзина
@@ -55,7 +70,6 @@ const MobileFooter = () => {
 					</div>
 				</a>
 			</Link>
-
 
 		</div>
 	);
