34c79c4d3edfcb35a6fdaf8ab6892d6bc04e5bd1
Sirius Sufiev <44986984+sc0der@users.noreply.github.com>
2022-02-22 10:39:56 +0400

    Update bw-client.sh

--- bw-client.sh
+++ bw-client.sh
@@ -1,10 +1,9 @@
-# export NVM_DIR="$HOME/.nvm" 
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-# nvm use 16 
+export NVM_DIR="$HOME/.nvm"   
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
+nvm use 16   
 npm i 
 echo "all packages installed"
-mv ./Layouts/ layouts/
 npm run build
 echo "app built"
 echo "starting to run app ....."
-npm start
+npm run pm2
