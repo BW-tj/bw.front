d5fe1ad5e808c2fbab2f7d1a955a2dc09958ee1d
DalerAlyamov <developer.daler@gmail.com>
2022-02-22 14:16:26 +0500

    +update

--- bw-client.sh
+++ bw-client.sh
@@ -1,6 +1,6 @@
-# export NVM_DIR="$HOME/.nvm"   
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
-# nvm use 16   
+export NVM_DIR="$HOME/.nvm"   
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
+nvm use 16   
 # npm i 
 echo "all packages installed"
 npm run build
