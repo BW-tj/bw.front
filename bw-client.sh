6ffb6d940908f564e3094214a3a0c620f31e76f9
DalerAlyamov <developer.daler@gmail.com>
2022-02-24 15:09:56 +0500

    remove log

--- bw-client.sh
+++ bw-client.sh
@@ -1,9 +1,8 @@
-export NVM_DIR="$HOME/.nvm"   
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
-nvm use 16   
-# npm i 
+npm i
+ 
 echo "all packages installed"
 npm run build
 echo "app built"
 echo "starting to run app ....."
-npm start
+
+npm run pm2
