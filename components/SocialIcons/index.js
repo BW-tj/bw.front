f3c9c2571e9bb540291b3630ec7282e670252cd2
DalerAlyamov <developer.daler@gmail.com>
2022-03-17 18:44:37 +0500

    .

--- components/SocialIcons/index.js
+++ components/SocialIcons/index.js
@@ -4,27 +4,40 @@ import Link from 'next/link';
 import { InstagramFill, FacebookFill, TelegramFill } from '../../icons';
 import classNames from 'classnames';
 
-const instagram = 'https://www.instagram.com/bw.tj/';
-const facebook = 'https://www.facebook.com/bw.tj/';
-const telegram = 'https://www.telegram.com/bw.tj/';
-
 const SocialIcons = () => {
+
+	const [data, setData] = React.useState({
+		email: "",
+		phone: "",
+		about: "",
+		telegram: "",
+		instagram: "",
+		facebook: "",
+	})
+
+	React.useEffect(() => {
+		fetch(process.env.NEXT_PUBLIC_HOST + '/contacts', { method: 'GET' })
+			.then(res => res.json())
+			.then(data => {
+				setData(data)
+			})
+	}, [])
 	return (
 		<div className={styles.root}>
 
-			<Link href={instagram}>
+			<Link href={data.instagram}>
 				<a className={classNames(styles.icon, styles.instagram)}>
 					<InstagramFill size={28} />
 				</a>
 			</Link>
 
-			<Link href={facebook}>
+			<Link href={data.facebook}>
 				<a className={classNames(styles.icon, styles.facebook)}>
 					<FacebookFill size={28} />
 				</a>
 			</Link>
 
-			<Link href={telegram}>
+			<Link href={data.telegram}>
 				<a className={classNames(styles.icon, styles.telegram)}>
 					<TelegramFill size={28} />
 				</a>
