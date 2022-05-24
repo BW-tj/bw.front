253f797dead0121358fc17d777df957e540f58be
ainizoda <ainizodam@gmail.com>
2022-05-24 16:45:15 +0500

    asd

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0ba5635f22db9b45"; }
+/******/ 		__webpack_require__.h = function() { return "350cc1263637336d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
